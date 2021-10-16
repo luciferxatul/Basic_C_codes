3ea8c779adb45e83a8f51d0ca6dcc3645cee2d43
Anjan Kumar Majee <92600394+godxpredator@users.noreply.github.com>
2021-10-16 10:13:29 +0530

    Update powfunction.c

--- powfunction.c
+++ powfunction.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-#include<math.h>
-int main()
-{
-
+int pow(int n,int p){
+    int res=1;
+    for(int i=1;i<=p;i++)
+        res=res*n;
+    return res;
+}
+int main(){
     printf("2 to the power 4 is %f", pow(2,4));
     return 0;
 }
